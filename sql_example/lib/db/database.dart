4200ad8ee21239384a145e46d35bd8e693311f1e
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-17 15:42:25 +0300

    Adding CRUD features to DBProvider

--- sql_example/lib/db/database.dart
+++ sql_example/lib/db/database.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+import 'package:sql_example/model/student.dart';
+
 class DBProvider {
   DBProvider._();
   static final DBProvider db = DBProvider._();
@@ -29,5 +31,48 @@ class DBProvider {
   Future<void> _createDB(Database db, int version) async {
     await db.execute(
         'CREATE TABLE $studentsTable($columnId INTEGER PRIMARY KEY AUTOINCREMENT, $columnName TEXT)');
+  }
+
+  /// READ
+  Future<List<Student>> getStudents() async {
+    Database db = await database;
+    final List<Map<String, dynamic>> studentsMapList =
+        await db.query(studentsTable);
+
+    final studentsList = <Student>[];
+
+    for (var studentsMap in studentsMapList) {
+      studentsList.add(Student.fromMap(studentsMap));
+    }
+
+    return studentsList;
+  }
+
+  // INSEART
+  Future<Student> insertStudent(Student student) async {
+    Database db = await database;
+    student.id = await db.insert(studentsTable, student.toMap());
+    return student;
+  }
+
+  // UPDATE
+  Future<int> updateStudent(Student student) async {
+    Database db = await database;
+    return await db.update(
+      studentsTable,
+      student.toMap(),
+      where: '$columnId = ?',
+      whereArgs: [student.id],
+    );
+  }
+
+  // DELETE
+  Future<int> deleteStudent(int id) async {
+    Database db = await database;
+    return await db.delete(
+      studentsTable,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
   }
 }
