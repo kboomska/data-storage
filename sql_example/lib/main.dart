faee5770f9b8e9c4a877ff65200f5e2f51904a42
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-17 12:54:50 +0300

    SQLite example project initialization

---
+++ sql_example/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'SQLite CRUD Example',
+      home: StudentPage(),
+    );
+  }
+}
+
+class StudentPage extends StatefulWidget {
+  const StudentPage({super.key});
+
+  @override
+  State<StudentPage> createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
