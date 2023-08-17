d4036e8d9f1fa543e8a4854f5ea383d20b8b949e
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-17 17:29:22 +0300

    Implementing DBProvider to StudentPage

--- sql_example/lib/main.dart
+++ sql_example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sql_example/db/database.dart';
+
+import 'package:sql_example/model/student.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +29,23 @@ class StudentPage extends StatefulWidget {
 class _StudentPageState extends State<StudentPage> {
   final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
   final _studentNameController = TextEditingController();
+
+  Future<List<Student>>? _studentList;
+  String _studentName = '';
+  bool isUpdate = false;
+  int studentIdForUpdate = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    updateStudentList();
+  }
+
+  void updateStudentList() {
+    setState(() {
+      _studentList = DBProvider.db.getStudents();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +78,9 @@ class _StudentPageState extends State<StudentPage> {
                       }
                       return null;
                     },
-                    onSaved: (newValue) {},
+                    onSaved: (newValue) {
+                      _studentName = newValue ?? '';
+                    },
                     controller: _studentNameController,
                     decoration: const InputDecoration(
                       focusedBorder: UnderlineInputBorder(
@@ -87,27 +109,131 @@ class _StudentPageState extends State<StudentPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (isUpdate) {
+                    if (_formStateKey.currentState!.validate()) {
+                      _formStateKey.currentState!.save();
+                      DBProvider.db
+                          .updateStudent(
+                        Student(id: studentIdForUpdate, name: _studentName),
+                      )
+                          .then((value) {
+                        setState(() {
+                          isUpdate = false;
+                        });
+                      });
+                    }
+                  } else {
+                    if (_formStateKey.currentState!.validate()) {
+                      _formStateKey.currentState!.save();
+                      DBProvider.db
+                          .insertStudent(Student(id: null, name: _studentName));
+                    }
+                  }
+                  _studentNameController.text = '';
+                  updateStudentList();
+                },
                 style: const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.green),
                   foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-                child: const Text('ADD'),
+                child: Text(
+                  (isUpdate ? 'UPDATE' : 'ADD'),
+                ),
               ),
               const SizedBox(
                 width: 10,
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _studentNameController.text = '';
+                  setState(() {
+                    isUpdate = false;
+                    studentIdForUpdate = 0;
+                  });
+                },
                 style: const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.red),
                   foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-                child: const Text('CLEAR'),
+                child: Text(
+                  (isUpdate ? 'CANCEL UPDATE' : 'CLEAR'),
+                ),
               ),
             ],
           ),
+          const Divider(
+            height: 5.0,
+          ),
+          Expanded(
+              child: FutureBuilder(
+            future: _studentList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return generateList(snapshot.data!);
+              }
+              if (snapshot.data == null || snapshot.data!.isEmpty) {
+                return const Text('No Data Found');
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          )),
         ],
+      ),
+    );
+  }
+
+  SingleChildScrollView generateList(List<Student> students) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: DataTable(
+          columns: const [
+            DataColumn(
+              label: Text('NAME'),
+            ),
+            DataColumn(
+              label: Text('DELETE'),
+            ),
+          ],
+          rows: students
+              .map(
+                (student) => DataRow(
+                  cells: [
+                    DataCell(
+                      Text(student.name),
+                      onTap: () {
+                        setState(() {
+                          isUpdate = true;
+                          studentIdForUpdate = student.id!;
+                        });
+                        _studentNameController.text = student.name;
+                      },
+                    ),
+                    DataCell(
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          DBProvider.db.deleteStudent(student.id!);
+                          updateStudentList();
+                        },
+                      ),
+                      onTap: () {
+                        setState(() {
+                          isUpdate = true;
+                          studentIdForUpdate = student.id!;
+                        });
+                        _studentNameController.text = student.name;
+                      },
+                    ),
+                  ],
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
--- sql_example/lib/model/student.dart
+++ sql_example/lib/model/student.dart
@@ -1,5 +1,5 @@
 class Student {
-  int id;
+  int? id;
   String name;
 
   Student({
